8d9a0a7c184dd30905841f0f18ef7013ce7c9eab
edgars <edgar@skalena.com>
2024-12-04 19:50:05 -0300

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/edgar/Documents/skalena/tech/sandbox-fazenda-mg/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/edgar/Documents/skalena/tech/sandbox-fazenda-mg/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/edgar/Documents/skalena/tech/sandbox-fazenda-mg/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/edgar/Documents/skalena/tech/sandbox-fazenda-mg/src/css/custom.css"),
+];
